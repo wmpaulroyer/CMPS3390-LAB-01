d3e2c244d16ef3d1c6399fb07245572383c36b73
William Royer <wmpaulroyer@gmail.com>
2025-08-26 12:18:39 -0700

    added even MORE MATH

--- app.cpp
+++ app.cpp
@@ -11,6 +11,8 @@ int main(){
 
     cout << x << "plus" << y << " is " << x+y << endl;
     cout << x << "minus" << y << " is " << x-y << endl;
+    cout << x << "divided by" << y << " is " << x/y << endl;
+    cout << x << "multiplied by" << y << " is " << x*y << endl;
     
    
     return 0;
