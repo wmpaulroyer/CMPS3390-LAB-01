6407153aeaa760b69d5ff6bbf4935515ea349758
William Royer <wmpaulroyer@gmail.com>
2025-08-28 12:28:18 -0700

    added math header

--- app.cpp
+++ app.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include "math.h"
 using namespace std;
 
 int main(){
     
-    int x = 10;
-    int y = 30;
+    int x = 30;
+    int y = 10;
 
     cout << "WELCOME TO MY APP!" << endl;
     cout << "I'm using git!" << endl;
 
-    cout << x << "plus" << y << " is " << x+y << endl;
-    cout << x << "minus" << y << " is " << x-y << endl;
-    cout << x << "divided by" << y << " is " << x/y << endl;
-    cout << x << "multiplied by" << y << " is " << x*y << endl;   
-    
-    cout << "This is a Git fututive test" << endl; 
-    
+    cout << x << " plus " << y << " is " << add(x,y) << endl;
+    cout << x << " minus " << y << " is " << subtract(x,y) << endl;
+    cout << x << " divided by " << y << " is " << divide(x,y) << endl;
+    cout << x << " multiplied by " << y << " is " << multiply(x,y) << endl;   
     
     return 0;
 }
